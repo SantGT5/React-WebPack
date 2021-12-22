5980ac6dfc70a18ffa571c5a7574e9f0c9bb6786
SantGT5 <gianspf@gmail.com>
2021-12-22 17:51:39 +0100

    Eslint config && Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import "./style.css";
-import IMAGE from "./react.png";
-import LOGO from "./logo.svg";
-import { ClickCounter } from "./ClickCounter";
+import './style.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-
   const num = 0
-
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV} { process.env.name }
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{' '}
+        {process.env.name}
       </h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200"></img>
       <img src={LOGO} alt="React Logo" width="300"></img>
       <ClickCounter />
     </>
-  );
-};
+  )
+}
